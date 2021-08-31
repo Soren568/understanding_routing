5e58dd966d2d114a5e31c9e40019e96755a00e47
Soren568 <sorheitmann@gmail.com>
2021-08-31 09:22:07 -0600

    initial commit

---
+++ server.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app =Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return "Hello World!"
+
+@app.route('/dojo')
+def dojo():
+    return "Dojo!"
+
+@app.route('/say/<string:name>')
+def say_hi(name):
+    return f"Hi {name}!"
+
+@app.route('/repeat/<int:num>/<string:word>')
+def repeat(num, word):
+    return num * word
+
+if __name__=="__main__":
+    app.run(debug=True)
